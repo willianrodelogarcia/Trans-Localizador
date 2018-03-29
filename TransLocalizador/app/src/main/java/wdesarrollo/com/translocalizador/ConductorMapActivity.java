5b940a33626d889d987aa657717f0980f119019f
Willian Rodelo <36023500+willianrodelogarcia@users.noreply.github.com>
2018-03-29 02:09:02 -0500

    Funciones con Mapas

--- TransLocalizador/app/src/main/java/wdesarrollo/com/translocalizador/ConductorMapActivity.java
+++ TransLocalizador/app/src/main/java/wdesarrollo/com/translocalizador/ConductorMapActivity.java
@@ -4,133 +4,54 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Build;
 import android.os.Looper;
-import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.view.Gravity;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.firebase.auth.FirebaseAuth;
 
-public class UsuarioMapActivity extends FragmentActivity implements OnMapReadyCallback {
+public class ConductorMapActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-
-    private ImageButton mMenu;
-    private DrawerLayout mDrawerLayout;
-    private NavigationView mNavigationView;
 
     Location mLastLocation;
     LocationRequest mLocationRequest;
     private FusedLocationProviderClient mFusedLocationClient;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_usuario_map);
+        setContentView(R.layout.activity_conductor_map);
+
+
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
 
-        mMenu = findViewById(R.id.btnMenu);
-
-        mDrawerLayout = findViewById(R.id.drawer_layout);
-
-        mNavigationView = findViewById(R.id.navi);
-
-
-        mMenu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mDrawerLayout.openDrawer(GravityCompat.START);
-            }
-        });
-
-        MenuItem item = mNavigationView.getMenu().getItem(0);
-        item.setChecked(true);
-
-        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
-
-                    case R.id.menu_mapa:
-                        item.setChecked(true);
-                        mDrawerLayout.closeDrawers();
-                        break;
-
-                    case R.id.menu_conf:
-                        //Toast.makeText(UsuarioMapActivity.this,"Configuracion",Toast.LENGTH_LONG).show();
-                        item.setChecked(true);
-                        mDrawerLayout.closeDrawers();
-                        break;
-                    case R.id.menu_acerca:
-                        //Toast.makeText(UsuarioMapActivity.this,"Acerca de",Toast.LENGTH_LONG).show();
-                        item.setChecked(true);
-                        mDrawerLayout.closeDrawers();
-                        break;
-                    case R.id.menu_cerrar:
-                        //Toast.makeText(UsuarioMapActivity.this,"Cerrar",Toast.LENGTH_LONG).show();
-                        FirebaseAuth.getInstance().signOut();
-                        Intent intent = new Intent(UsuarioMapActivity.this,MainActivity.class);
-                        startActivity(intent);
-                        finish();
-
-                        //item.setChecked(true);
-                        //mDrawerLayout.closeDrawers();
-                        break;
-
-                    default:
-                        break;
-                }
-                return true;
-            }
-        });
-
-
     }
 
-
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @SuppressLint("RestrictedApi")
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -151,8 +72,8 @@ public class UsuarioMapActivity extends FragmentActivity implements OnMapReadyCa
         }
         mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
         mMap.setMyLocationEnabled(true);
-    }
 
+    }
     LocationCallback mLocationCallback = new LocationCallback(){
         @Override
         public void onLocationResult(LocationResult locationResult) {
@@ -168,7 +89,6 @@ public class UsuarioMapActivity extends FragmentActivity implements OnMapReadyCa
             }
         }
     };
-
     private void checkLocationPermission(){
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
             if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
@@ -179,12 +99,12 @@ public class UsuarioMapActivity extends FragmentActivity implements OnMapReadyCa
                         .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                ActivityCompat.requestPermissions(UsuarioMapActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+                                ActivityCompat.requestPermissions(ConductorMapActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
                             }
                         }).create().show();
 
             }else{
-                ActivityCompat.requestPermissions(UsuarioMapActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+                ActivityCompat.requestPermissions(ConductorMapActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
             }
         }
     }
--- TransLocalizador/app/src/main/java/wdesarrollo/com/translocalizador/MainActivity.java
+++ TransLocalizador/app/src/main/java/wdesarrollo/com/translocalizador/MainActivity.java
@@ -1,15 +1,22 @@
 package wdesarrollo.com.translocalizador;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,22 +30,44 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //FireBase Auth
         setContentView(R.layout.activity_main);
+        //FireBase Auth
+
 
         mAuth = FirebaseAuth.getInstance();
 
         fireAuthStateListener = new FirebaseAuth.AuthStateListener() {
-            FirebaseUser user= FirebaseAuth.getInstance().getCurrentUser();
+
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
                 if(user!=null){
                     //Manda al mapa del Usuario para ver a los conductores
-                    Intent intent = new Intent(MainActivity.this,UsuarioMapActivity.class);
-                    startActivity(intent);
-                    finish();
+                    String tipoUsuario = user.getUid();
+                    DatabaseReference refTransporte = FirebaseDatabase.getInstance().getReference().child("Transportes").child(tipoUsuario);
+                    //DatabaseReference refUsuario = FirebaseDatabase.getInstance().getReference().child("Usuarios").child(tipoUsuario);
+
+                    //Toast.makeText(MainActivity.this,"REF "+refConductor.getKey()+ " REFU "+refUsuario.getKey(),Toast.LENGTH_LONG).show();
+
+                    if(tipoUsuario.equals(refTransporte.getKey())){
+
+                        Toast.makeText(MainActivity.this,"Es un conductor",Toast.LENGTH_LONG).show();
+                       /* Intent intent = new Intent(MainActivity.this,ConductorMapActivity.class);
+                        startActivity(intent);
+                        finish();*/
+                    }else{
+                        Toast.makeText(MainActivity.this,"Es un Usuario",Toast.LENGTH_LONG).show();
+                    }
+
+                        /*Intent intent = new Intent(MainActivity.this,UsuarioMapActivity.class);
+                        startActivity(intent);
+                        finish();*/
+
+
+
+
+
                 }
 
             }
@@ -58,7 +87,17 @@ public class MainActivity extends AppCompatActivity {
         mInicio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                final String correo = mUsuario.getText().toString();
+                final String password = mContraseña.getText().toString();
 
+                mAuth.signInWithEmailAndPassword(correo,password).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(!task.isSuccessful()){
+                            Toast.makeText(MainActivity.this,"Error al Iniciar Sesion",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
             }
         });
 
